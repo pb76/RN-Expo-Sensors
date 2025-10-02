ffe4aeaf99741184e527e692bb13dfa3e650cb48
Patrik Bernhardsson <pb76@users.noreply.github.com>
2025-10-02 13:10:06 +0200

    Lägg till Sensors-komponenten och uppdatera App.jsx för att inkludera den

---
+++ components/Sensors.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Sensors() {
+  return (
+    <View style={styles.container}>
+      <Text>Här kommer vi visa info från sensorer</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
