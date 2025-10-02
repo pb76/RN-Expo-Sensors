70263bef4fa13b029cc8afc328037c65e8244ded
Patrik Bernhardsson <pb76@users.noreply.github.com>
2025-10-02 13:15:42 +0200

    Bytte namn på komponenten från Sensors till Sensorer och uppdaterade App.jsx för att använda den nya komponenten

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Sensors from "./components/Sensors";
+import Sensorer from "./components/Sensorer";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Sensors />
+      <Sensorer />
       <StatusBar style="auto" />
     </View>
   );
--- components/Sensorer.jsx
+++ components/Sensorer.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
+//https://docs.expo.dev/versions/latest/sdk/sensors/
+import * as Sensors from "expo-sensors";
 
-export default function Sensors() {
+export default function Sensorer() {
   return (
     <View style={styles.container}>
       <Text>Här kommer vi visa info från sensorer</Text>
